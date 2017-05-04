a15c3a38c64fbc45793e47b27bc90b96089dc927
ccurtin <work@christopherjamescurtin.com>
2017-05-04 13:16:54 -0400

    Update PropTypes w/ npm prop-types. Add Notes to redux file

--- src/modules/Blog/redux.js
+++ src/modules/Blog/redux.js
@@ -9,6 +9,12 @@ const FETCH_ARTICLE_REQUEST = 'blog/FETCH_ARTICLE_REQUEST'
 const FETCH_ARTICLE_SUCCESS = 'blog/FETCH_ARTICLE_SUCCESS'
 const FETCH_ARTICLE_FAILURE = 'blog/FETCH_ARTICLE_FAILURE'
 
+
+/*
+  REDUCERS (the data responsible for updating the Redux store)
+  all reducers are fired off when an action is dispatched. It's up to you to determine what data to return based on the action.type.
+*/
+
 const articles = (state = [], action) => {
   switch (action.type) {
   case FETCH_ARTICLES_SUCCESS:
@@ -31,11 +37,11 @@ const article = (state = null, action) => {
   return state
 }
 
-const reducer = combineReducers({
-  articles,
-  article
-})
 
+
+/*
+  ACTION CREATOR (creates actions)
+*/
 const fetchAction = (url, types) => dispatch => {
   const [REQUEST, SUCCESS, FAILURE] = types
 
@@ -63,6 +69,9 @@ const fetchAction = (url, types) => dispatch => {
     )
 }
 
+/*
+  ACTION CREATORs - fires off fetchAction which in turn triggers the "Actions" (actions get "dispatched")
+*/
 export const fetchArticles = () => fetchAction(
   '/api/articles',
   [FETCH_ARTICLES_REQUEST, FETCH_ARTICLES_SUCCESS, FETCH_ARTICLES_FAILURE]
@@ -73,6 +82,26 @@ export const fetchArticle = slug => fetchAction(
   [FETCH_ARTICLE_REQUEST, FETCH_ARTICLE_SUCCESS, FETCH_ARTICLE_FAILURE]
 )
 
+
+
+
+/*
+  EXAMPLE: a simple action (no action creator needed.)
+  say, trigger from a component, when a user sumbits form
+*/
+/*export const simpleAction__addToMessageBoard = (username, category, theMessage) => {
+    return {
+        type: ADD_MESSAGE_TO_BOARD,
+        username: username,
+        category: category,
+        theMessage: theMessage
+    }
+}       }
+*/
+
+
+
+
 export const getArticles = state => state.blog.articles
 
 export const getArticle = (state, slug) => {
@@ -82,5 +111,16 @@ export const getArticle = (state, slug) => {
 }
 
 export const getLoading = state => state.blog.article === 'loading'
+
+
+/*
+  Combines reducer functions together, to manage a specific slice of the state.
+  In this case, these 2 reducers are combined and placed in another "combineReducers()" method in the root at "src/reducer.js"
+*/
+
+const reducer = combineReducers({
+  articles,
+  article
+})
 
 export default reducer
